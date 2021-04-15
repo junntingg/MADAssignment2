29aa93d64520431ccdc65cbc4dca964b18ed8208
junntingg <chewjt-wl18@student.tarc.edu.my>
2021-04-15 22:08:08 +0800

    Starting screen

---
+++ app/src/main/java/com/example/madassignment/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.madassignment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mHandler: Handler
+    lateinit var mRunnable: Runnable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        fun startMainActivity() {
+            mRunnable = Runnable {
+                startActivity(Intent(this, MainLogin::class.java))
+                finish()
+            }
+
+            mHandler = Handler()
+
+            mHandler.postDelayed(mRunnable, 5000)
+        }
+        startMainActivity()
+    }
+    override fun onStop() {
+        super.onStop()
+        mHandler.removeCallbacks(mRunnable)
+    }
+}
---
+++ app/src/main/java/com/example/madassignment/MainLogin.kt
@@ -0,0 +1,12 @@
+package com.example.madassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_login)
+    }
+
+}
