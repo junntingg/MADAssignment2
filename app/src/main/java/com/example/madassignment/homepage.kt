d4bc6cf4b4315e9501198f2b7fd7ebf19330665d
Maeqi <chungmq-wm18@student.tarc.edu.my>
2021-04-17 01:48:28 +0800

    mqHomepage

---
+++ app/src/main/java/com/example/madassignment/homepage.kt
@@ -0,0 +1,12 @@
+package com.example.madassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+        supportActionBar?.hide()
+    }
+}
