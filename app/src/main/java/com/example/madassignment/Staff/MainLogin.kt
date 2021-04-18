d7d328719f20bc553b9a11bdb2339592bd867142
junntingg <chewjt-wl18@student.tarc.edu.my>
2021-04-19 01:01:56 +0800

    jttest

--- app/src/main/java/com/example/madassignment/Staff/MainLogin.kt
+++ app/src/main/java/com/example/madassignment/Staff/MainLogin.kt
@@ -3,8 +3,16 @@ package com.example.madassignment.Staff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.madassignment.R
+import com.example.madassignment.data.UserDao
+import com.example.madassignment.data.UserDatabase
+import com.example.madassignment.data.UserViewModel
 import kotlinx.android.synthetic.main.activity_main_login.*
+
+private lateinit var mUserViewModel: UserViewModel
 
 class MainLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +22,18 @@ class MainLogin : AppCompatActivity() {
 
     fun loginLogin(view: View){
 
-        if(staffIDLogin.text.toString() == passwordLogin.text.toString()){
+        val userDao = UserDatabase.getDatabase(applicationContext)?.userDao()
 
+        val email = staffIDLogin.text.toString()
+        val password = passwordLogin.text.toString()
 
+        if(email == null || password == null){
+
+            Toast.makeText(this, "Please fill up all fields.", Toast.LENGTH_LONG).show()
 
         }else{
+            mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+            val user = mUserViewModel.staffLogin(email, password)
 
         }
 
